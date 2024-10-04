772cfbf19f2fb8ec503e58dd38cb31c28fdfb3af
bnj35 <benjamin.auger6@etu.univ-lorraine.fr>
2024-10-04 22:37:41 +0200

    patch error deploy

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,9 +4,7 @@ export default function NotFound() {
     return (
         <main>
         <div className='flex justify-center items-center'>
-            <p className='px-10 mx-12 w-auto text-[6rem] my-44 tracking-wide leading-tight '>
-    <strong>'404'
-    </strong> Not Found</p>
+            <p className='px-10 mx-12 w-auto text-[6rem] my-44 tracking-wide leading-tight '><strong>404</strong> Not Found</p>
         </div>
         </main>
     );
