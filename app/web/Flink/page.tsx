23a6e8f274d4d47eb4ade851b7d624c338a4783d
bnj35 <benjamin.auger6@etu.univ-lorraine.fr>
2024-10-04 14:36:18 +0200

    add description flink

--- app/web/Flink/page.tsx
+++ app/web/Flink/page.tsx
@@ -6,12 +6,12 @@ export default function Home() {
     return (
         <div className='sm:m-12 md:m-16'>
             <ImageTextSection params={{ImgSrc: '/web/flink.jpg', 
-                ImgAlt: 'Ordi', 
-                TextTitle: 'Laptop', 
-                TextContent: 'Throughout my journey in the world of graphic design, I have had the opportunity to work on multiple client projects. Primarily in the music industry, I have created numerous posters and album covers in various styles and genres. This experience has provided me with significant skills and knowledge, which are valuable assets in a professional setting.'}}/>
+                ImgAlt: 'Flink', 
+                TextTitle: 'Flink', 
+                TextContent: 'Flink is a project in development with my classmates that functions as a SaaS platform designed to gather all of a person\'s links, social networks, and websites onto a single page, similar to Linktree or Bento.me. The key advantages of this application will be its free access, shortened URLs, and the ability to generate QR codes to simplify access during virtual reality experiences.'}}/>
                 
 
-                <Text params={{Title: 'Client Order', Text: 'Throughout my journey in the world of graphic design, I have had the opportunity to work on multiple client projects. Primarily in the music industry, I have created numerous posters and album covers in various styles and genres. This experience has provided me with significant skills and knowledge, which are valuable assets in a professional setting.'}}/>
+                <Text params={{Title: 'Technical details', Text: 'The project currently consists of a PHP backend using the Laravel framework and a frontend built with Nuxt/Vue.js. From a design perspective, we opted for a slightly neo-brutalist style with vibrant colors to create a modern and distinctive look. Roles within the team are not fixed, which means I may work on both the backend and frontend, making this project a unique and motivating experience.'}}/>
         </div>
     )
 }
